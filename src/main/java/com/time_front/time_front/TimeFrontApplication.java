eecb4995071df55287dd3ab897a589f5dcfdbe44
GuiXiansong <GuiXiansong666@gmial.com>
2021-04-30 21:11:52 +0800

    第一次提交

---
+++ src/main/java/com/time_front/time_front/TimeFrontApplication.java
@@ -0,0 +1,13 @@
+package com.time_front.time_front;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TimeFrontApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(TimeFrontApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/time_front/time_front/TimeFrontApplicationTests.java
@@ -0,0 +1,13 @@
+package com.time_front.time_front;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TimeFrontApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
